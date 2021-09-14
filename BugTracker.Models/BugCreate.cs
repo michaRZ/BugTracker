3153fc3b1ed19b5fd7cd1b4c9e529deea1d2735a
Elijah Wood <elijah.v.wood@gmail.com>
2021-09-14 10:16:48 -0400

    added BugCreate to Models

---
+++ BugTracker.Models/BugCreate.cs
@@ -0,0 +1,24 @@
+﻿using BugTracker.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+    public class BugCreate
+    {
+        [Required]
+        public string BugName { get; set; }
+        [Required]
+        public string BugDescription { get; set; }
+        [Required]
+        public DateTimeOffset IdentifiedUtc { get; set; }
+        [Required]
+        public int ProjectId { get; set; }
+        public int? AssignedTo { get; set; }
+        public Priority? Priority { get; set; }
+    }
+}
