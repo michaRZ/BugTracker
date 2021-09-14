03681a861719e1c440492026a2f577b5533da610
Elijah Wood <elijah.v.wood@gmail.com>
2021-09-14 10:25:57 -0400

    added BugStageEdit to models

---
+++ BugTracker.Models/BugStageEdit.cs
@@ -0,0 +1,17 @@
+﻿using BugTracker.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+    public class BugStageEdit
+    {
+        public int BugId { get; set; }
+        public Stage Status { get; set; }
+        public bool ActiveProblem { get; set; }
+        public Priority Priority { get; set; }
+    }
+}
