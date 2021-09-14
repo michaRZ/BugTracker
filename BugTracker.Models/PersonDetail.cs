391cff42183a43981451b38df7adc675bf31a2c0
Anh Vo <vohoangvananh81@gmail.com>
2021-09-14 10:32:35 -0400

    add model

---
+++ BugTracker.Models/PersonDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+    public class PersonDetail
+    {
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public int? AssignedProject { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
