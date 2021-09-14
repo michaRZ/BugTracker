a0fa8e3b5ed38b651dda367c30136bb9763f1811
Elijah Wood <elijah.v.wood@gmail.com>
2021-09-14 10:23:02 -0400

    added BugEdit Model

--- BugTracker.Models/BugEdit.cs
+++ BugTracker.Models/BugEdit.cs
@@ -1,37 +1,27 @@
-﻿using System;
+﻿using BugTracker.Data;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BugTracker.Data
+namespace BugTracker.Models
 {
-    public enum Stage {New =1, Assign, Open, Fixed, Test, Verified, Closed, Reopen, Duplicate, Deferred, Rejected}
-    public enum Priority {None, Low, Medium, High}
-    public class Bug
+    public class BugEdit
     {
-        [Key]
         public int BugId { get; set; }
-        [Required]
+
         public string BugName { get; set; }
-        [Required]
         public string BugDescription { get; set; }
-        [Required]
         public DateTimeOffset IdentifiedUtc { get; set; }
-        [Required]
         public int ProjectId { get; set; }
         public int? AssignedTo { get; set; }
         public Stage Status { get; set; }
-        public bool ActiveProblem { get; set; } = true;
+        public bool ActiveProblem { get; set; }
         public Priority Priority { get; set; }
 
         public DateTimeOffset ExpectedResolutionUTC { get; set; }
         public DateTimeOffset? ActualResolutionUTC { get; set; }
         public string ResolutionSummary { get; set; }
-        public DateTimeOffset? CreatedUTC { get; set; }
-        public DateTimeOffset? ModifiedUTC { get; set; }
-        public string ModifiedBy { get; set; }
     }
 }
