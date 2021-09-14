c1f759ee384b00895a1e37de01350d881099a608
Elijah Wood <elijah.v.wood@gmail.com>
2021-09-14 16:07:51 -0400

    added methods to Bug Service

--- BugTracker.Services/BugService.cs
+++ BugTracker.Services/BugService.cs
@@ -43,17 +43,59 @@ namespace BugTracker.Services
                 var query = ctx.Bugs.Where(e => e.ActiveProblem)
                     .Select
                     (
-                        e=>new BugListItem
+                        e => new BugListItem
                         {
-                            BugId=e.BugId,
-                            BugName=e.BugName,
-                            IdentifiedUtc=e.IdentifiedUtc,
-                            ProjectId=e.ProjectId,
-                            AssignedTo=e.AssignedTo,
-                            Status=e.Status
+                            BugId = e.BugId,
+                            BugName = e.BugName,
+                            IdentifiedUtc = e.IdentifiedUtc,
+                            ProjectId = e.ProjectId,
+                            AssignedTo = e.AssignedTo,
+                            Status = e.Status
                         }
                     );
                 return query.ToArray();
+            }
+        }
+        public IEnumerable<BugListItem> GetBugsByProjectId(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx.Bugs.Where(e => e.ProjectId == id && e.ActiveProblem).Select
+                    (
+                        e => new BugListItem
+                        {
+                            BugId = e.BugId,
+                            BugName = e.BugName,
+                            IdentifiedUtc = e.IdentifiedUtc,
+                            ProjectId = e.ProjectId,
+                            AssignedTo = e.AssignedTo,
+                            Status = e.Status
+
+                        }
+                    );
+                return query.ToArray();
+            }
+        }
+        public BugDetail GetBugById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Bugs.Single(e => e.BugId == id && e.ActiveProblem);
+
+                return 
+                 new BugDetail 
+                {
+                    BugId=entity.BugId,
+                    BugName=entity.BugName,
+                    BugDescription=entity.BugDescription,
+                    ProjectId=entity.ProjectId,
+                    Status=entity.Status,
+                    ActiveProblem=entity.ActiveProblem,
+                    Priority=entity.Priority,
+                    IdentifiedUtc=entity.IdentifiedUtc,
+                    CreatedUTC=entity.CreatedUTC,
+                    ModifiedUTC=entity.ModifiedUTC
+                };
             }
         }
     }
