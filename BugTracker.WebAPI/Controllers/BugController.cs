571ecc94868ec21332c013d1a2e4247341bf9085
Elijah Wood <elijah.v.wood@gmail.com>
2021-09-15 11:38:42 -0400

    added GetBugs method [HttpGet] to Controller

--- BugTracker.WebAPI/Controllers/BugController.cs
+++ BugTracker.WebAPI/Controllers/BugController.cs
@@ -1,13 +1,34 @@
-﻿using System;
+﻿using BugTracker.Services;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace BugTracker.WebAPI.Controllers
 {
+    [Authorize]
     public class BugController : ApiController
     {
+        private BugService CreateBugService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var bugService = new BugService(userId);
+
+            return bugService;
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            var bugService = CreateBugService();
+            var bugs = bugService.GetBugs();
+
+            return Ok(bugs);
+        }
+
     }
 }
