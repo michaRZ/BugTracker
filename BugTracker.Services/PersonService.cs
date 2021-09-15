34b8c899007d3d22dcb5689108ecbcea84262df0
Anh Vo <vohoangvananh81@gmail.com>
2021-09-15 12:19:02 -0400

    create, and get

--- BugTracker.Services/PersonService.cs
+++ BugTracker.Services/PersonService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BugTracker.Data;
+using BugTracker.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,39 @@ namespace BugTracker.Services
         public PersonService(Guid userId)
         {
             _userId = userId;
+        }
+        
+        public bool CreatePerson(PersonCreate model)
+        {
+            var entity =
+                new Person()
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    Role = model.Role
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.People.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<PersonListItem> GetPeople()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx.People
+                    //.Where 
+                    .Select(e => new PersonListItem
+                    {
+                        PersonId = e.PersonId,
+                        Name = e.Name
+                    }
+                    );
+                return query.ToArray();
+            }
         }
 
     }
